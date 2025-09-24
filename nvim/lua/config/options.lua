5501130fe8271ec262099139c97480d0b6d3edd7
Jose <josemaurette1234@gmail.com>
2025-09-23 21:28:14 -0400

    "Copy to clipboard"

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,7 @@ vim.opt.number = true
 -- vim.opt.cursorline = false
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
 
 -- Fix input delay for keybindings
 vim.opt.timeoutlen = 300
